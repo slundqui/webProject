72e55c34a96e9fb212d9fa7423ba0933f9bb48e4
C.Sun <sun428@gmail.com>
2013-12-02 01:59:51 -0700

    UI (username, turn)  update in game

--- GameRoom/src/java/edu/nmt/cs/itweb/GameServer.java
+++ GameRoom/src/java/edu/nmt/cs/itweb/GameServer.java
@@ -12,6 +12,8 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
 import javax.json.stream.JsonParser;
 import javax.servlet.http.HttpSession;
 import javax.websocket.CloseReason;
@@ -50,11 +52,11 @@ public class GameServer {
         HttpSession httpSession = (HttpSession) conf.getUserProperties().get(HttpSession.class.getName());
         String seat = (String)conf.getUserProperties().get("SeatIndex");
         if(seat != null) {
-            int seatIndex = Integer.parseInt(seat);
+            int seatIndex = Integer.parseInt(seat) - 1;
             String user = (String)httpSession.getAttribute("LOGIN_USER");
-            if(user.equals(WebSocketServer.inGame[seatIndex-1])) {
+            if(user.equals(WebSocketServer.inGame[seatIndex])) {
                 authorized = true;
-                peers[getTableIndex(seatIndex-1)].add(session);
+                peers[getTableIndex(seatIndex)].add(session);
             }
         }
         if(!authorized) {
@@ -63,27 +65,39 @@ public class GameServer {
     }
     
     @OnMessage
-    public void onMessage(Session session, String msg) throws IOException, EncodeException {
-        JsonParser parser = Json.createParser(new StringReader(msg));
-        parser.next();
-        parser.next();
-        parser.next();
-        int action = parser.getInt();
-        parser.next();
-        parser.next();
-        int seatIndex = parser.getInt();
-        parser.next();
-        parser.next();
-        int x = parser.getInt();
-        parser.next();
-        parser.next();
-        int y = parser.getInt();
-        switch (action) {
-            case WebSocketMessage.GOMOKU_PUT_STONE:
-                int tableIndex = getTableIndex(seatIndex - 1);
-                games[tableIndex].putStone(x, y, getSeatColor(seatIndex));
-                broadcastMessage(tableIndex, msg);
-                break;
+    public void onMessage(Session session, String msg) {
+        try {
+            JsonParser parser = Json.createParser(new StringReader(msg));
+            parser.next();
+            parser.next();
+            parser.next();
+            int action = parser.getInt();
+            parser.next();
+            parser.next();
+            int seatIndex = parser.getInt() - 1;
+            int tableIndex = getTableIndex(seatIndex);
+            parser.next();
+            parser.next();
+            int x = parser.getInt();
+            parser.next();
+            parser.next();
+            int y = parser.getInt();
+            switch (action) {
+                case WebSocketMessage.GOMOKU_ENTER_GAME:
+                    sendSeatsUser(session, seatIndex);
+                    if(peers[tableIndex].size() == 2) {
+                        broadcastGameReady(tableIndex);
+                    }
+                    break;
+                case WebSocketMessage.GOMOKU_PUT_STONE:
+                    games[tableIndex].putStone(x, y, getSeatColor(seatIndex));
+                    broadcastMessage(tableIndex, msg);
+                    break;
+                default:
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
         }
     }
     
@@ -105,14 +119,18 @@ public class GameServer {
         }
     }
     
-    private void broadcastMessage(int tableIndex, String msg) throws IOException, EncodeException {
+    private void broadcastMessage(int tableIndex, String msg) {
         for (Session peer : peers[tableIndex]) {
             sendMessage(peer, msg);
         }
     }
     
-    private void sendMessage(Session session, String msg) throws IOException, EncodeException {
-        session.getBasicRemote().sendObject(msg);
+    private void sendMessage(Session session, String msg){
+        try {
+            session.getBasicRemote().sendObject(msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
     private int getTableIndex(int seatIndex){
@@ -124,5 +142,33 @@ public class GameServer {
     
     private int getSeatColor(int seatIndex) {
         return seatIndex % 2 + 1;
+    }
+    
+    private void sendSeatsUser(Session session, int seatIndex) {
+        String leftUser;
+        String rightUser;
+        if(seatIndex % 2 == 0){
+            leftUser = WebSocketServer.seatsUser[seatIndex];
+            rightUser = WebSocketServer.seatsUser[seatIndex+1];
+        }
+        else {
+            leftUser = WebSocketServer.seatsUser[seatIndex-1];
+            rightUser = WebSocketServer.seatsUser[seatIndex];
+        }
+        JsonObject jobj = Json.createObjectBuilder()
+                .add("action", WebSocketMessage.GOMOKU_PLAYERS)
+                .add("left", leftUser)
+                .add("right", rightUser)
+                .build();
+        String msg = jobj.toString();
+        sendMessage(session, msg);
+    }
+    
+    private void broadcastGameReady(int tableIndex) {
+        JsonObject jobj = Json.createObjectBuilder()
+                .add("action", WebSocketMessage.GOMOKU_GAME_READY)
+                .build();
+        String msg = jobj.toString();
+        broadcastMessage(tableIndex, msg);
     }
 }
